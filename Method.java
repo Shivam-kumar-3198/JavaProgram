e17ad5e47a2e84415877bd79c18b912ee8726857
Shivam <thisisshivamkumar8840@gmail.com>
2023-10-11 16:05:51 +0530

    Changed Value

---
+++ Method.java
@@ -0,0 +1,32 @@
+package JavaProgram;
+class Cal 
+{
+    void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    void add(int a, float b){
+        System.out.println(a+b);
+    }
+
+    void add(float a, float b){
+        System.out.println(a+b);
+    }
+    
+    void add(double a , double b){
+        System.out.println(a+b);
+    }
+
+}
+
+class Method
+{
+    public static void main(String args[])
+    {
+        Cal obj = new Cal();
+        obj.add(23,45);
+        obj.add(23,45.5f);
+        obj.add(23.45,45.67);
+        obj.add(34.567,45.678);
+    }
+}
